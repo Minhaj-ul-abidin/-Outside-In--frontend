0f2593fb13891e59385f4ecf4535f9e249f1b915
Minhaj-ul-abidin <minhaj3labidin@gmail.com>
2021-07-21 02:07:53 +0530

    Add unit tests for ListRestaurants , specify restraunts should render , specify first render of restaurants should call loadRestaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,4 +1,16 @@
+import {useEffect} from 'react';
 
-export const RestaurantList = () => <div>RestaurantList</div>;
+export const RestaurantList = ({loadRestaurants, restaurants}) => {
+  useEffect(() => {
+    loadRestaurants();
+  }, [loadRestaurants]);
 
+  return (
+    <ul>
+      {restaurants.map(res => (
+        <li key={res.id}>{res.name}</li>
+      ))}
+    </ul>
+  );
+};
 export default RestaurantList;
